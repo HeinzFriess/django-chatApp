91251528e7f527274624959397341825389c7319
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2023-05-10 17:18:30 +0200

    model extended

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
+    if request.method == 'POST':
+        print("Received data" + request.POST['textmessage'])
     return render(request, 'chat/index.html', {'username' : 'Heinz'})
 
 
