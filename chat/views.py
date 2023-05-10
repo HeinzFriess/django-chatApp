6e75cc00f2b1a932799db48213a2aa09cfa199ca
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2023-05-10 21:41:08 +0200

    login implemented

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,25 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .models import Chat, Message
+from django.contrib.auth import authenticate, login
 
 def index(request):
     if request.method == 'POST':
         print("Received data" + request.POST['textmessage'])
-    return render(request, 'chat/index.html', {'username' : 'Heinz'})
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages' : chatMessages})
+
+def loginView(request):
+    if request.method == 'POST':
+        user = authenticate(username = request.POST.get('username'), password = request.POST.get('password'))
+        if user:
+            login(request, user)
+            return HttpResponseRedirect('/chat/')
+        else:
+            return render(request, 'login.html', {'wrongPassword': True})
+
+    return render(request, 'login.html', {'messages' : 'Hallo'})
 
 
