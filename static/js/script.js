ccc81e987a19e7bb6d02b49eb099e33d12b44a93
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2023-05-22 17:13:06 +0200

    script bug

---
+++ static/js/script.js
@@ -0,0 +1,52 @@
+function getDate() {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"];
+    let dateObj = new Date();
+    let month = dateObj.getUTCMonth() + 1; //months from 1-12
+    let day = dateObj.getUTCDate();
+    let year = dateObj.getUTCFullYear();
+
+    return monthNames[month-1] + " " + day + ", " + year;
+  }
+
+//   async function sendMessage() {
+//     let fd = new FormData();
+//     let token = '{{ csrf_token }}';
+//     fd.append('textmessage', messageField.value);
+//     fd.append('csrfmiddlewaretoken', token);
+//     try {
+//       document.getElementById('messageContainer').innerHTML += `
+//          <div class="mdl-card" id="toDelete">
+//           <div class="mdl-card__title">
+//             <span class="color-gray"> [${getDate()}] : {{ request.user.first_name}}</span>
+//           </div>
+//           <div class="mdl-card__media color-gray" >
+//             ${messageField.value}
+//           </div>
+//         </div>
+//       `;
+//       let response = await fetch('/chat/', {
+//         method: 'POST',
+//         body: fd
+//       })
+//       let json = await response.json();
+
+//       console.log(json);
+
+//       document.getElementById('toDelete').remove();
+
+//       document.getElementById('messageContainer').innerHTML += `
+//         <div class="mdl-card">
+//           <div class="mdl-card__title">
+//             <span class="color-gray"> [${getDate()}] : {{ request.user.first_name}}</span>
+//           </div>
+//           <div class="mdl-card__media color-gray" >
+//             ${messageField.value}
+//           </div>
+//         </div>
+//       `;
+//       messageField.value = '';
+//     } catch (error) {
+//       console.error(e);
+//     }
+//   }
