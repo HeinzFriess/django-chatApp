8304bf26f61c25737a9d705653e5e10b512732d8
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2023-05-25 18:14:26 +0200

    design change

--- first_django_chat_app/settings.py
+++ first_django_chat_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e@^o90*qyn1guc@h*t+r$-&jl3xjgq6d(26(hrey@=x0#td%89
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.2.41']
 
 
 # Application definition
